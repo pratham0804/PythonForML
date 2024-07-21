d4859fc6feb704f4d07c7c57979c4091ea587781
pratham0804 <149933759+pratham0804@users.noreply.github.com>
2024-07-21 13:02:00 +0000

    basic printing and indentation

---
+++ Python/basics.py
@@ -0,0 +1,46 @@
+print("Hello github codespace, Long time no see");
+
+
+# indentation  in python is very important , as it defines the scope of code
+if 5 > 2 :
+    print("five is greater than 2");
+
+# if we remove this indentation , it shows Indentationerror
+#if 5 > 2 : 
+#print("five is greater than 2");
+
+
+#* the number of spaces is up to programmer , but it has to be atleast one.
+
+
+
+
+## this does not throw error
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+
+# this does
+
+#if 5 > 2:
+# print("Five is greater than two!")
+#        print("Five is greater than two!")
+ 
+ # indentation error
+
+
+
+##########  COMMENTS #########
+
+#** No multiline commment  syntax in python , use # in every line
+
+ 
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+#* writing multiline string (non-executable , since it is not assigned to any variable)
